0d9aa92641aa8d0950472801d32c8e758429837c
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-06-24 11:44:20 +0530

    add input

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ class App extends React.Component {
     render(){
         return(
             <div>
-                <img src={logo} />
+                <img src={logo} height="100" width="120" className="logo" />
+                <h1 className="app-title">ToDo App</h1>
+                <div className="container">
+                    Add an Item... <br/>
+                    <input type="text" className="input-text" placeholder="Write a ToDo"></input>
+                    <button className="app-btn">Add ToDo</button>
+                </div>
             </div>
         );
     }
